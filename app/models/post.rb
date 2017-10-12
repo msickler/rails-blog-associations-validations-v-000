89eb6969aaa25dc3e56a9ed0034c8be8fcc7ba0d
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 13:43:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  has_many :tags 
+  has_many :tags
   belongs_to :user
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+  has_many :posts 
+  has_many :users, through: :posts
 end
