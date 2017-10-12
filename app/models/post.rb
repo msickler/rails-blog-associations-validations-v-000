ccfa39530d7854c66ec4f2ca9d82452237a51df6
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 13:56:37 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   has_many :tags, through: :post_tags
   has_many :post_tags
-  belongs_to :user
+  
 
   validates :name, presence: true
   validates :content, presence: true
